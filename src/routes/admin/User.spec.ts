03b7c666e6fe5fa8f2555b901c67c3cbf5f0233d
Natarajan <natarajanmca11@gmail.com>
2016-05-20 19:56:23 +1200

    unit test for router level

--- src/routes/admin/User.spec.ts
+++ src/routes/admin/User.spec.ts
@@ -1,28 +1,28 @@
-//import * as request from 'supertest';
+import * as request from 'supertest';
 import {WebServer } from '../../Core'; //, Request, Response, NextFunction
 import {AppConfig} from '../../config';
 import * as route from '../../routes';
 
-//const baseUrl = 'http://127.0.0.1:' + AppConfig.ApiPort;
+const baseUrl = 'http://127.0.0.1:' + AppConfig.ApiPort;
 const server = new WebServer(AppConfig);
 server.Init();
 server.AddApiRouting('/', route);
 server.Start();
 
-// describe('/User', () => {
-//     it('POST /FindById', async (done) => {
-//         request(baseUrl)
-//             .post('/user/FindById')
-//             .send({
-//                 'Id': 1
-//             })
-//             .set('Accept', 'application/json')
-//             //.expect('Content-Type', /json/)
-//             //.expect('Content-Length', '15')
-//             .end(function (err, res) {
-//                 if (err) throw err;
-//                 console.log(res);
-//             })
-//             .expect(200, done);
-//     });
-// });
+describe('/User', () => {
+    it('POST /FindById', (done) => {
+        request(baseUrl)
+            .post('/user/FindById')
+            .send({
+                'Id': 1
+            })
+            .set('Accept', 'application/json')
+            .expect('Content-Type', /json/)
+            //.expect('Content-Length', '15')
+            .expect(200)
+            .end(function (err: any, res: any) {
+                if (err) throw err;
+                done();
+            });
+    });
+});
